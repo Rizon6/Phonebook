cc1b220c684dbe5a62a25ae64193bf70b1c4ab03
Rizon6 <98629896+Rizon6@users.noreply.github.com>
2023-06-12 13:37:07 -0700

    Add files via upload

---
+++ Phonebook.java
@@ -0,0 +1,13 @@
+//Rizon Takabe
+//4-30-2023
+//CS 145
+//Assignment 1: Phonebook
+import java.util.*;
+
+public class Phonebook{
+   public static void main (String [] args){
+      Scanner input = new Scanner(System.in);
+      BookManager M = new BookManager();
+      M.menu(input);  
+   }
+}
